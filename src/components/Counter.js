0e087e13af9c738d12b4441cf3699e455ca608bc
Six Eggs <sixeggs0303@gmail.com>
2020-12-07 16:40:14 +0800

    [ReactCounter][Marcus Luk]Feat: Created basic counter

---
+++ src/components/Counter.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            number: 0,
+        };
+    }
+
+    onIncrease = () => {
+        this.setState((prevState) => ({ number: prevState.number + 1 }));
+    }
+
+    onDecrease = () => {
+        this.setState((prevState) => ({ number: prevState.number - 1 }));
+    }
+
+
+    render() {
+        return (
+            <section>
+                <input type="button" value="+" onClick={this.onIncrease} />
+                <span>{this.state.number}</span>
+                <input type="button" value="-" onClick={this.onDecrease} />
+            </section>
+        )
+    }
+}
